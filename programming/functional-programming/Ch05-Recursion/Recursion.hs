ff1906258c8579075b12489f97a39f6dded5eaff
Artem Lytvyn <alytvyn@redhat.com>
2026-07-09 20:58:00 +0100

    done ch05

---
+++ programming/functional-programming/Ch05-Recursion/Recursion.hs
@@ -0,0 +1,44 @@
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' x y
+    | x <= 0 = []
+    | otherwise = y:replicate' (x - 1) y
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x:take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallerSorted = quicksort [el | el <- xs, el <= x]
+        biggerSorted = quicksort [el | el <- xs, el > x]
+    in smallerSorted ++ [x] ++ biggerSorted
